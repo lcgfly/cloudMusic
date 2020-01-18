ebbd414e3b2718660e6a0e1880f96d0023d4484f
lcg <694781147@qq.com>
2020-01-18 22:34:02 +0800

    修改配置文件

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ Vue.config.productionTip = false;
 //全局样式
 import "./assets/css/deviceAdapter/index.less";
 import "./assets/css/reset.less";
+import "vant/lib/index.css";
+//vant组件
+import { Tab, Tabs, Button } from "vant";
+
+Vue.use(Button);
+Vue.use(Tab).use(Tabs);
 
 new Vue({
   router,
