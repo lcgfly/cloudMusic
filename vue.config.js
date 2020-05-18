2f542b8c0c87d1de068520cefbe28c3c8d50e45a
lcg <694781147@qq.com>
2020-05-18 11:51:04 +0800

    feat:配置文更新件

--- vue.config.js
+++ vue.config.js
@@ -10,52 +10,17 @@ module.exports = {
                   '^/api': '' // 这是一个通配符，设置完了之后每个接口都要在前面加上/api（特别注意这一点）
                 }
               }
-            // '/banner':{
-            //     target:'http://localhost:3000',
-            //     changeOrigin:true
-            // },
-            // '/login':{
-            //     target:'http://localhost:3000',
-            //     changeOrigin:true
-            // },
-            // '/logout':{
-            //     target:'http://localhost:3000',
-            //     changeOrigin:true
-            // },
-            // '/recommend':{
-            //     target:'http://localhost:3000',
-            //     changeOrigin:true
-            // },
-            // '/lyric':{
-            //     target:'http://localhost:3000',
-            //     changeOrigin:true
-            // },
-            // '/song':{
-            //     target:'http://localhost:3000',
-            //     changeOrigin:true
-            // },
-            // '/playlist':{
-            //     target:'http://localhost:3000',
-            //     changeOrigin:true
-            // },
-            // '/top':{
-            //     target:'http://localhost:3000',
-            //     changeOrigin:true
-            // },
-            // '/toplist':{
-            //     target:'http://localhost:3000',
-            //     changeOrigin:true
-            // },
-            // '/artists':{
-            //     target:'http://localhost:3000',
-            //     changeOrigin:true
-            // },
-            // '/artist':{
-            //     target:'http://localhost:3000',
-            //     changeOrigin:true
-            // }
-
         }
     },
-    lintOnSave: false
+    chainWebpack:config=>{
+      config.module
+        .rule('images')
+        .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
+        .use('image-webpack-loader')
+        .loader('image-webpack-loader')
+        .options({ bypassOnDebug: true })
+        .end()
+    },
+    lintOnSave: false,
+    productionSourceMap: false  //打包后不生成sourcemap文件
 }
